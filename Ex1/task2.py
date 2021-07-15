f440b76e7ed9f565fb96b1ef05fcacdcd001d8b8
PonMorin <66424414+PonMorin@users.noreply.github.com>
2021-07-15 12:22:33 +0700

    Create task2.py

---
+++ Ex1/task2.py
@@ -0,0 +1,5 @@
+#2 รับค่าชื่อ นามสกุล และแสดงผลชื่อนามสกุลโดยขั้นด้วยเครื่องหมายดังนี้ **
+#   a.	ตัวอย่างการแสดงผล My**Name**Is**James**Cameron
+name = input('Enter your name: ')
+surname = input('Enter your surname: ')
+print('My**Name**Is**{}**{}'.format(name,surname))
