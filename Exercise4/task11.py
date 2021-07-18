348993dc73779782f678d06e54d77b37dccb8ba5
PonMorin <66424414+PonMorin@users.noreply.github.com>
2021-07-18 10:34:05 +0700

    Create task11.py

---
+++ Exercise4/task11.py
@@ -0,0 +1,8 @@
+num = int(input(''))
+for row in range(num):
+    for column in range(row + 1):
+        if column == 0 or column == row:
+            print('*',end='')
+        else:
+            print(end=' ')
+    print()
