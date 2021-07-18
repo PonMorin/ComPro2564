3d692a10b2cd36224c5f7cd404ac1bdcc3fd618a
PonMorin <66424414+PonMorin@users.noreply.github.com>
2021-07-18 10:33:17 +0700

    Create task10.py

---
+++ Exercise4/task10.py
@@ -0,0 +1,14 @@
+n = int(input('Enter: '))
+for row in range(n):
+  for space in range(n - row - 1):
+    print(end=' ')
+  for column in range(2*row + 1):
+    print('*', end='')
+  print()
+
+for row2 in range(n-1, 0, -1):
+  for space in range(n - row2):
+    print(end=' ')
+  for column in range(2*row2 - 1):
+    print('*', end='')
+  print()
