3e0be79f5a1acfabbe7c4269272b9c591d0e4f36
PonMorin <66424414+PonMorin@users.noreply.github.com>
2021-07-22 13:11:33 +0700

    Create task14.py

---
+++ Exercise5/task14.py
@@ -0,0 +1,17 @@
+#14
+str1 = input('Enter: ')
+special_character = [' ', '#', '@', '!', '%', '$', ',', ':', ';', '&']
+count_upper = 0
+count_lower = 0
+count_special = 0
+
+for i in range(len(str1)):
+    if str1[i].isupper():
+        count_upper += 1
+    elif str1[i].islower():
+        count_lower += 1
+    elif str1[i] in special_character:
+        count_special += 1
+print('Uppercase character number is',count_upper)
+print('Lowercase character number is',count_lower)
+print('Special character number is',count_special)
