cd591878ddd73bbe239810f0860c27a8dbf2ff93
PonMorin <66424414+PonMorin@users.noreply.github.com>
2021-07-22 13:07:25 +0700

    Delete task11.py

--- Exercise4/task11.py
+++
@@ -1,8 +0,0 @@
-num = int(input(''))
-for row in range(num):
-    for column in range(row + 1):
-        if column == 0 or column == row:
-            print('*',end='')
-        else:
-            print(end=' ')
-    print()
